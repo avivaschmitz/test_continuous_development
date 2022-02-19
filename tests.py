1560a970ad5ad340e97f786eb485caf083feb1e8
avivaschmitz <39923753+avivaschmitz@users.noreply.github.com>
2022-02-19 13:59:45 -0800

    fixed syntax errors in tests.py

--- tests.py
+++ tests.py
@@ -1,10 +1,12 @@
 import unittest
 
 class TestCase(unittest.TestCase):
+
   
-  def test1(self):
-    self.assertTrue(True)
-    
+    def test1(self):
+        self.assertTrue(True)
+
 
 if __name__ == '__main__':
   unittest.main()
+
